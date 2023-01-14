226b08106e22f034adb09de00889dcc0152c9ab6
Roman Filiyutich <roman.filiyutich@sap.com>
2023-01-14 21:25:02 +0300

    24. Add to module-info.java for each implementation module: provides [interface] with [implementation]

--- jmp-cloud-bank-impl/src/main/java/module-info.java
+++ jmp-cloud-bank-impl/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module jmp.cloud.bank.impl {
     requires transitive jmp.bank.api;
     requires jmp.dto;
     exports com.romanfill.modules.bank.impl;
+    provides com.romanfill.modules.api.Bank with com.romanfill.modules.bank.impl.DefaultBank;
 }
--- jmp-cloud-service-impl/src/main/java/module-info.java
+++ jmp-cloud-service-impl/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module jmp.cloud.service.impl {
     requires transitive jmp.service.api;
     requires jmp.dto;
     exports com.romanfill.modules.cloud.service.impl;
+    provides com.romanfill.modules.service.api.Service with com.romanfill.modules.cloud.service.impl.DefaultService;
 }

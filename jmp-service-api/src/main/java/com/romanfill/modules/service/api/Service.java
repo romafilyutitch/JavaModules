f079575ef444ebe58e0f86c7b333a73eef0678a2
Roman Filiyutich <roman.filiyutich@sap.com>
2023-01-14 21:03:28 +0300

    18. Add a new default double getAverageUsersAge() method for Service interface which uses getAllUsers(), LocalDate.now(), ChronoUnit and mapToLong()

--- jmp-service-api/src/main/java/com/romanfill/modules/service/api/Service.java
+++ jmp-service-api/src/main/java/com/romanfill/modules/service/api/Service.java
@@ -4,6 +4,8 @@ import com.romanfill.modules.dto.BankCard;
 import com.romanfill.modules.dto.Subscription;
 import com.romanfill.modules.dto.User;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +13,11 @@ public interface Service {
     void subscribe(BankCard bankCard);
     Optional<Subscription> getSubscriptionByBankCardNumber(String cardNumber);
     List<User> getAllUsers();
+
+    default double getAverageUsersAge() {
+        return getAllUsers().stream()
+                .mapToLong(user -> ChronoUnit.YEARS.between(user.getBirthday(), LocalDate.now()))
+                .average()
+                .getAsDouble();
+    }
 }

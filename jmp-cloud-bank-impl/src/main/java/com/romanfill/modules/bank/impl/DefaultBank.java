76d31d996760015dd3578b0e563926fa28c40755
Roman Filiyutich <roman.filiyutich@sap.com>
2023-01-14 21:22:13 +0300

    23. Reimplement createBankCard() with method reference.

--- jmp-cloud-bank-impl/src/main/java/com/romanfill/modules/bank/impl/DefaultBank.java
+++ jmp-cloud-bank-impl/src/main/java/com/romanfill/modules/bank/impl/DefaultBank.java
@@ -4,17 +4,21 @@ import com.romanfill.modules.api.Bank;
 import com.romanfill.modules.dto.*;
 
 import java.util.Random;
+import java.util.function.BiFunction;
 
 public class DefaultBank implements Bank {
     private Random rand = new Random();
+    private BiFunction<String, User, BankCard> creditCardSupplier = CreditBankCard::new;
+    private BiFunction<String, User, BankCard> debitCardSupplier = DebitBankCard::new;
+
 
     @Override
     public BankCard createBankCard(User user, BankCardType cardType) {
         final String cardNumber = Integer.toString(rand.nextInt(100));
         if (cardType == BankCardType.CREDIT) {
-            return new CreditBankCard(cardNumber, user);
+            return creditCardSupplier.apply(cardNumber, user);
         } else if (cardType == BankCardType.DEBIT) {
-            return new DebitBankCard(cardNumber, user);
+            return debitCardSupplier.apply(cardNumber, user);
         }
         throw new IllegalArgumentException();
     }
